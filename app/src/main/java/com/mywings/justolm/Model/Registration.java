d3fbac19ea0b85fd883a16e6c68cb1d43639ed86
chavantr <tatyabhau.chavan@gmail.com>
2016-05-19 15:33:57 +0530

    - new added registration.

---
+++ app/src/main/java/com/mywings/justolm/Model/Registration.java
@@ -0,0 +1,136 @@
+package com.mywings.justolm.Model;
+
+/**
+ * Created by Tatyabhau Chavan on 5/19/2016.
+ */
+public class Registration {
+
+    //region Variables
+    private static Registration ourInstance;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String dateOfBirth;
+    private String gender;
+    private String profession;
+    private String address;
+    private String country;
+    private String pinCode;
+    private String email;
+    private String mobileNumber;
+    private String securityCode;
+    private String password;
+    //endregion
+
+
+    public static Registration getInstance() {
+        if (null == ourInstance) {
+            ourInstance = new Registration();
+        }
+        return ourInstance;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(String pinCode) {
+        this.pinCode = pinCode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getSecurityCode() {
+        return securityCode;
+    }
+
+    public void setSecurityCode(String securityCode) {
+        this.securityCode = securityCode;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- app/src/main/java/com/mywings/justolm/Registration.java
+++ app/src/main/java/com/mywings/justolm/Registration.java
@@ -92,7 +92,6 @@ public class Registration extends AppCompatActivity {
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-
             int year = c.get(Calendar.YEAR);
             int month = c.get(Calendar.MONTH);
             int day = c.get(Calendar.DAY_OF_MONTH);
